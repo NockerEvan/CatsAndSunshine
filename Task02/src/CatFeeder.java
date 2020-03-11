0efe1c8b34188e7705646cfc205f6de8e5e0b6ef
Angelina Grigorchuk <angelina_grigorchuk@epam.com>
2020-03-11 19:59:50 +0400

    Started task 2

---
+++ Task02/src/CatFeeder.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CatFeeder {
+    private int foodIndex;
+    private boolean isReadyToEat;
+//    BufferedReader reader = new InputStreamReader();
+    float f = 0.1F;
+
+    private void printFoodMenu() {
+        System.out.println("These are available food types:");
+        System.out.println();
+
+    }
+
+    private void printFoodReaction(String foodType) {
+
+    }
+
+    private void feedTheCat() {
+
+    }
+}
