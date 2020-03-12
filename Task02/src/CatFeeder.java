7358e875b338cff18ebc6625987fdb44654d95ca
Angelina Grigorchuk <angelina_grigorchuk@epam.com>
2020-03-12 15:10:37 +0400

    done Task 2

--- Task02/src/CatFeeder.java
+++ Task02/src/CatFeeder.java
@@ -1,23 +1,71 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CatFeeder {
-    private int foodIndex;
-    private boolean isReadyToEat;
-//    BufferedReader reader = new InputStreamReader();
-    float f = 0.1F;
+    private int foodIndex = 0;
+    private int favouriteFoodIndex;
+    private String[] food;
+    private String chosenFood;
+    private boolean isCatAlive = true;
+    private boolean isPresent = false;
+
+    private void chooseFavouriteFood() {
+        this.favouriteFoodIndex = (int) (Math.random() * food.length);
+    }
 
     private void printFoodMenu() {
         System.out.println("These are available food types:");
-        System.out.println();
-
+        for (String s: food) {
+            System.out.println("- " + s);
+        }
+        System.out.println("Please select one to feed the cat");
     }
 
-    private void printFoodReaction(String foodType) {
+    private void printFoodReaction() throws IOException {
+        isChosenFoodAvailable();
+        if (isPresent) {
+            if (chosenFood.equals(food[favouriteFoodIndex].toLowerCase())) {
+                System.out.println("Myao! That was very tasty!");
+                feedTheCat();
+            } else {
+                System.out.println("Frrr! I want something else!");
+            }
+        } else {
+            System.out.println("There is no such food available");
+        }
+    }
 
+    private void isChosenFoodAvailable() throws IOException  {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        chosenFood = reader.readLine().toLowerCase();
+
+        for(String s:food) {
+            if(chosenFood.equals(s.toLowerCase())){
+                isPresent=true;
+                break;
+            }
+        }
     }
 
     private void feedTheCat() {
+        if (foodIndex < 2) {
+            foodIndex++;
+        } else {
+            System.out.println("Kaboom! Tha cat has exploded." + "\n" + "Do not feed you cat so much next time.");
+            isCatAlive = false;
+        }
+    }
 
+    public void runCatFeeder() throws IOException {
+        printFoodMenu();
+        while (isCatAlive) {
+            printFoodReaction();
+        }
+    }
+
+    public CatFeeder(String[] food) {
+        this.food = food;
+        chooseFavouriteFood();
     }
 }
--- Task02/src/Main.java
+++ Task02/src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String[] catFood = {"Kibble", "Champion", "Whiskas", "Pollo"};
+        CatFeeder feeder = new CatFeeder(catFood);
+        feeder.runCatFeeder();
     }
 }
